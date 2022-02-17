26928f3eafec6a5f569dfdadfdf903257a1d179e
Trenten <trenten.grede@gmail.com>
2022-02-17 07:56:12 -0600

    Revert "Set up the first basic style for the app component"
    This reverts commit a8f53efd127060ce37ebae7db3be6a7bb58c1dbb.

--- brand-page/src/App.tsx
+++ brand-page/src/App.tsx
@@ -1,19 +1,9 @@
-import styled from 'styled-components'
-
 function App() {
   return (
-    <BrandPage>
+    <div>
 
-    </BrandPage>
+    </div>
   );
 }
-
-const BrandPage = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 100% !important;
-  overflow-x: hidden !important;
-`
 
 export default App;
