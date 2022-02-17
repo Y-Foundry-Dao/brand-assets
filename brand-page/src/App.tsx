a8f53efd127060ce37ebae7db3be6a7bb58c1dbb
Trenten <trenten.grede@gmail.com>
2022-02-17 07:56:12 -0600

    Set up the first basic style for the app component

--- brand-page/src/App.tsx
+++ brand-page/src/App.tsx
@@ -1,9 +1,19 @@
+import styled from 'styled-components'
+
 function App() {
   return (
-    <div>
+    <BrandPage>
 
-    </div>
+    </BrandPage>
   );
 }
+
+const BrandPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 100% !important;
+  overflow-x: hidden !important;
+`
 
 export default App;
